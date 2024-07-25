c89446e085f92cd39b133453b72210921ccc495c
gxlpes <guilhermxlopes@gmail.com>
2024-07-25 08:44:35 -0300

    fix: remove unnecessary fields from locker

--- src/main/java/com/lockers/api/models/LockerModel.java
+++ src/main/java/com/lockers/api/models/LockerModel.java
@@ -1,77 +1,58 @@
-package com.lockers.api.models;
+    package com.lockers.api.models;
 
-import com.lockers.api.misc.LockerId;
-import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+    import com.lockers.api.misc.LockerId;
+    import jakarta.persistence.*;
+    import org.hibernate.annotations.CreationTimestamp;
+    import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.Instant;
+    import java.time.Instant;
 
-@Entity
-@Table(name = "lockers", uniqueConstraints = @UniqueConstraint(columnNames = {"number", "sector"}))
-public class LockerModel {
-    @EmbeddedId
-    private LockerId id;
+    @Entity
+    @Table(name = "lockers", uniqueConstraints = @UniqueConstraint(columnNames = {"number", "sector"}))
+    public class LockerModel {
+        @EmbeddedId
+        private LockerId id;
 
-    @CreationTimestamp
-    private Instant createdAt;
+        @CreationTimestamp
+        private Instant createdAt;
 
-    @UpdateTimestamp
-    private Instant updatedAt;
+        @UpdateTimestamp
+        private Instant updatedAt;
 
-    @ManyToOne
-    @JoinColumn(name = "student_grr", nullable = false)
-    private StudentModel student;
+        @ManyToOne
+        @JoinColumn(name = "student_grr", nullable = false)
+        private StudentModel student;
 
-    private String sector;
+        public LockerId getId() {
+            return id;
+        }
 
-    private int number;
+        public void setId(LockerId id) {
+            this.id = id;
+        }
 
-    public LockerId getId() {
-        return id;
+        public Instant getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(Instant createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public Instant getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public void setUpdatedAt(Instant updatedAt) {
+            this.updatedAt = updatedAt;
+        }
+
+        public StudentModel getStudent() {
+            return student;
+        }
+
+        public void setStudent(StudentModel student) {
+            this.student = student;
+        }
+
     }
-
-    public void setId(LockerId id) {
-        this.id = id;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public StudentModel getStudent() {
-        return student;
-    }
-
-    public void setStudent(StudentModel student) {
-        this.student = student;
-    }
-
-    public String getSector() {
-        return sector;
-    }
-
-    public void setSector(String sector) {
-        this.sector = sector;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-}
